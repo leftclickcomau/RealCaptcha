f599da69fedb8a9e227b6d7cb073866b428bc880
Ben New <ben@leftclick.com.au>
2013-03-22 22:53:13 +0800

    Create demo.

---
+++ demo/captcha-check.php
@@ -0,0 +1,11 @@
+<?php
+/*!
+ * This file is a part of RealCaptcha human verification system.
+ * Copyright (c) Ben New, Leftclick.com.au
+ * See the LICENSE and README files in the main source directory for details.
+ */
+
+/**
+ * RealCaptcha code check demonstration.
+ */
+
