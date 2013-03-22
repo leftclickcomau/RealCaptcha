9b71edbdc05e2c620c1f02b773d83fbe5177368e
Ben New <ben@leftclick.com.au>
2013-03-22 20:21:15 +0800

    Documentation and acknowledgement.

--- src/RealCaptcha/RealCaptcha.php
+++ src/RealCaptcha/RealCaptcha.php
@@ -3,10 +3,18 @@
  * This file is a part of RealCaptcha human verification system.
  * Copyright (c) Ben New, Leftclick.com.au
  * See the LICENSE and README files in the main source directory for details.
+ *
+ * Partly based on "CaptchaSecurityImages.php" by Simon Jarvis, copyright 2006, updated: 07/02/07
+ * http://www.white-hat-web-design.co.uk/articles/php-captcha.php
  */
 
 namespace RealCaptcha;
 
+/**
+ * RealCaptcha provides an easy to implement captcha mechanism for any type of web form.
+ *
+ * @package RealCaptcha
+ */
 class RealCaptcha {
 
 	//-- Constants --------------------
@@ -149,6 +157,11 @@ class RealCaptcha {
 
 	//-- Internal Methods --------------------
 
+	/**
+	 * Initialise the session, if it needs to be and it is possible.
+	 *
+	 * @throws \RuntimeException
+	 */
 	protected function initSession() {
 		/** @noinspection PhpVoidFunctionResultUsedInspection Issue with PhpStorm 6.0 */
 		switch (session_status()) {
